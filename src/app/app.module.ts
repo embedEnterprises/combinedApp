0f9c198359de064c48d164c689e16df5cad70274
praneshnangare <praneshnangare10@gmail.com>
2023-07-02 10:30:44 +0530

    PWA version completed with service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PhaserComponent } from './phaser/phaser.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 const routes: Routes = [
   {path:'' , component:PhaserComponent},
@@ -23,6 +25,12 @@ const routes: Routes = [
         BrowserModule,
         HttpClientModule,
         RouterModule.forRoot(routes),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: environment.production,
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        }),
     ],
     bootstrap: [AppComponent],
     exports: [PhaserComponent]
