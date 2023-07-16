f5e1d449057c48f58a063222fb8b026235a8b425
praneshnangare <praneshnangare10@gmail.com>
2023-07-16 19:31:16 +0530

    background added and pedal retraction

--- src/app/phaser/components/break.ts
+++ src/app/phaser/components/break.ts
@@ -6,6 +6,10 @@ export default class Break extends Phaser.GameObjects.Mesh {
   debug;
   zone;
   ptrId;
+  initialVal;
+  pedalTwin;
+  pedalSpeed: number = 500;
+
   constructor (scene, conf){
     super(scene, conf.x, conf.y , 'break',);
     Phaser.Geom.Mesh.GenerateGridVerts({
@@ -21,7 +25,7 @@ export default class Break extends Phaser.GameObjects.Mesh {
                 .setInteractive();
     scaleToGameZone(scene, this.zone, conf.zw, conf.zh)
     setPos(scene, this.zone, conf.zx, conf.zy)
-
+    this.initialVal = this.modelRotation.x;
     this.zone.on('pointerdown', this.handlePointerDown , this);
   }
 
@@ -29,16 +33,14 @@ export default class Break extends Phaser.GameObjects.Mesh {
     this.ptrId = e.id;
     this.scene.input.on('pointermove' , this.handlePointerMove, this);
     this.scene.input.on('pointerup' , this.handlePointerUp, this);
-    console.log("hello")
   }
 
-  
   private handlePointerMove(e) {
     if(this.ptrId == e.id) {
-      console.log("there")
       const rotateRate = 1,
-      xuplim = 1,
-      xlowlim = 0
+            xuplim = 1,
+            xlowlim = 0
+      console.log(this.modelRotation.x);
       let val = e.velocity.y * (rotateRate / 600);
       let ldiff = xlowlim - this.modelRotation.x;
       let udiff = xuplim - this.modelRotation.x;
@@ -47,14 +49,29 @@ export default class Break extends Phaser.GameObjects.Mesh {
       }
     }
   }
+
   private handlePointerUp(e) {
     if (this.ptrId == e.id) {
       this.scene.input.off('pointermove')
       this.scene.input.off('pointerup')
       this.ptrId == null;
+
+      this.pedalTwin = this.scene.tweens.addCounter({
+        from: this.modelRotation.x,
+        to: this.initialVal,
+        duration: Math.abs(this.modelRotation.x * this.pedalSpeed),
+        repeat: 0,
+        ease: Phaser.Math.Easing.Quadratic.Out,
+        onUpdate: (tween) => 
+        {
+            let val = tween.getValue()
+            this.modelRotation.x = val;
+        },
+        
+    });
+
     }
   }
-
 
   public scaleToGameW(obj, per) {
     obj.displayWidth = Number(this.scene.sys.game.config.width) * per;
